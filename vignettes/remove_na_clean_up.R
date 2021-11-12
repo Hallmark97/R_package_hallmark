0185ed8d77ae0a9ded79d536ebdb6e23ad18c508
hallmark97 <kyle.hallmark@selu.edu>
2021-11-12 00:07:08 +0000

    Project two input and output

---
+++ vignettes/remove_na_clean_up.R
@@ -0,0 +1,5 @@
+This clean up will removes any "non values" from the data set so it 
+does not mess with the graphs being used and does cause anything to become
+construde. The output would be a clean data set with no missing data. I
+would also create a new dataset with the cleaned up data so nothing 
+gets messed up.
